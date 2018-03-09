b77d458164dd4dce453b20cb3959b073d4674c53
David Lechner <david@lechnology.com>
2018-03-09 15:59:16 -0600

    improve setup.cmake

--- setup.cmake
+++ setup.cmake
@@ -1,8 +1,13 @@
-# Create the build directory and initalize it
+# Create the build directory and initialize it
+#
+# run this file with `cmake -P setup.cmake`
+
 
 file (MAKE_DIRECTORY build)
 
 execute_process (
-    COMMAND ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Debug ..
+    COMMAND ${CMAKE_COMMAND}
+        -DCMAKE_BUILD_TYPE=Debug
+        ..
     WORKING_DIRECTORY build
 )
